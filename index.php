c0c5505816be83c88d5b2e35dd17bc46ab5e1a9c
mickael keita <keitamickael@gmail.com>
2024-10-02 16:45:59 +0200

    initialisation du projet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo app</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="public/js/index.js" defer></script>
+</head>
+
+<body>
+</body>
+<header></header>
+<main class="content"></main>
+<footer></footer>
+
+</html>
