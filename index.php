aa96f9139ba43f713e85fe83c3bf1947ad7b1f05
mickael keita <keitamickael@gmail.com>
2024-10-04 09:18:49 +0200

    mis en place du style

--- index.php
+++ index.php
@@ -5,14 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo app</title>
+
+    <!-- CSS -->
     <link rel="stylesheet" href="public/css/style.css">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+    <!-- JS -->
     <script src="public/js/index.js" defer></script>
 </head>
 
 <body>
 </body>
-<header></header>
-<main class="content"></main>
-<footer></footer>
+<div class="container">
+    <header>
+        <div class="logo">Todo</div>
+    </header>
+    <main class="content">
+        <div class="todo-container">
+            <h1>Ma Todo</h1>
+            <div class="todo-form"></div>
+            <div class="togo-list"></div>
+        </div>
+    </main>
+    <footer>Todo app 2024 © tous droit réservés</footer>
+</div>
 
 </html>
