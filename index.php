092cb7160bd7bf67c19174b7ff5b802a4362fb70
mickael keita <keitamickael@gmail.com>
2024-10-07 12:41:26 +0200

    recuperation des todos et stockage dans un fichier json

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+const ERROR_REQUIRED = "Veuillez renseigner une todo";
+const ERROR_TOO_SHORT = "veuillez saisir au moin 5 caractères";
+
+$filename = __DIR__ . "\data\\todo.json";
+$error = "";
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $todoValue = $_POST["value"] ?? "";
+
+    if (!$todoValue) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todoValue) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+}
+
+if (!$error) {
+    $todos = [...$todos, [
+        'name' => $todoValue,
+        'done' => false,
+        'id' => time()
+    ]];
+    file_put_contents($filename, json_encode($todos));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,6 +52,9 @@
                 <input type="text" name="value" id="">
                 <button class="btn btn-primary">Ajouter</button>
             </form>
+            <?php if ($error): ?>
+                <p class="text-danger"><?= $error ?></p>
+            <?php endif; ?>
             <div class="togo-list"></div>
         </div>
     </main>
